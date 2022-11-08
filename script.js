94d2a256710c00276645974218764abc47bb64c4
Corky Seitz <corky.e.seitz@gmail.com>
2022-11-08 14:58:28 -0500

    add todo, cross out todo, js

--- script.js
+++ script.js
@@ -3,13 +3,18 @@ const todoList = document.querySelector("#todo-list");
 
 form.addEventListener("submit", function(e) {
     e.preventDefault();
+    console.log(e);
     const newToDo = document.querySelector("#todo");
     const newLi = document.createElement("li");
     newLi.innerText = newToDo.value;
+    
+    todoList.append(newLi);
+    form.reset();
 
-
-
-todoList.append(newLi);
-form.reset();
+    newLi.addEventListener("click", function(e){
+        console.log(e);
+        e.target.style.textDecoration = "line-through";
+    })
 
 })
+
