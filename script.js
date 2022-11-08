34b59f516cfc3bf6f0dda81d0af9a5e3ec97d6fa
Corky Seitz <corky.e.seitz@gmail.com>
2022-11-08 15:11:17 -0500

    add delete button

--- script.js
+++ script.js
@@ -6,15 +6,24 @@ form.addEventListener("submit", function(e) {
     console.log(e);
     const newToDo = document.querySelector("#todo");
     const newLi = document.createElement("li");
+    const newBtn = document.createElement("button");
     newLi.innerText = newToDo.value;
+    newBtn.innerText = "Delete";
     
+    newLi.append(newBtn);
     todoList.append(newLi);
     form.reset();
 
     newLi.addEventListener("click", function(e){
         console.log(e);
         e.target.style.textDecoration = "line-through";
-    })
+    });
 
-})
+    todoList.addEventListener("click", function(e) {
+        console.log(e);        
+        if (e.target.tagName === "BUTTON") {
+            e.target.parentElement.remove();
+        }
+    });
+});
 
