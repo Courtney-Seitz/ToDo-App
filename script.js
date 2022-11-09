bb93223f8afbc8fc44daa0fb0b692a046706b0a9
Corky Seitz <corky.e.seitz@gmail.com>
2022-11-09 11:55:17 -0500

    localstorage js somewhat working

--- script.js
+++ script.js
@@ -1,34 +1,37 @@
+document.addEventListener("DOMContentLoaded", function() {
 const form = document.querySelector("form");
 const todoList = document.querySelector("#todo-list");
 
 const savedToDos = JSON.parse(localStorage.getItem("todos")) || [];
 for (let i = 0; i < savedToDos.length; i++) {
-    let newToDo = document.querySelector("#todo");
     let newLi = document.createElement("li");
-    newLi.innerText = savedToDos[i].newToDo;
-    newLi.isCompleted - savedToDos[i].isCompleted ? true : false;
+    newLi.innerText = savedToDos[i].todo;
+    newLi.isCompleted = savedToDos[i].isCompleted ? true : false;
     if (newLi.isCompleted) {
         newLi.style.textDecoration = "line-through";
     } 
-    const newBtn = document.createElement("button");
+    todoList.append(newLi);
+console.log(newLi);
+    let newBtn = document.createElement("button");
     newBtn.innerText = "Delete";
     newLi.append(newBtn);
-    todoList.append(newLi);
 }
 
 form.addEventListener("submit", function(e) {
     e.preventDefault();
-    const newToDo = document.querySelector("#todo");
-    const newLi = document.createElement("li");
-    newLi.innerText = newToDo.value;
-    const newBtn = document.createElement("button");
+    let newLi = document.createElement("li");
+    let newToDo = document.getElementById("todo").value;
+    newLi.innerText = newToDo;
+    newLi.isCompleted = false;
+    form.reset();
+    todoList.append(newLi);
+    let newBtn = document.createElement("button");
     newBtn.innerText = "Delete";
     newLi.append(newBtn);
-    todoList.append(newLi);
-    form.reset();
 
-    savedToDos.push({ newToDo: newLi.innerText, isCompleted: false });
+    savedToDos.push({ todo: newToDo, isCompleted: false });
     localStorage.setItem("todos", JSON.stringify(savedToDos));
+    console.log(savedToDos);
 
     newLi.addEventListener("click", function(e){
         let clickedListItem = e.target;
@@ -42,10 +45,12 @@ form.addEventListener("submit", function(e) {
         }
     });
 
-    todoList.addEventListener("click", function(e) {      
-        if (e.target.tagName === "BUTTON") {
-            e.target.parentElement.remove();
-        }
-    });
 });
 
+todoList.addEventListener("click", function(e) {      
+    if (e.target.tagName === "BUTTON") {
+        e.target.parentElement.remove();
+    }
+});
+
+});
